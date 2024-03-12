48847d167f4c81d2632ed7714ece4aa273a4b717
Om Yadav <omydv3@gmail.com>
2024-03-12 23:39:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl bg-rose-300 p-4 text-center'>Om Yadav Portfolio</h1>
+    </div>
+  )
+}
+
+export default App
